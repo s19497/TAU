dcb7bfa09b47d3effe52bd23f296ba14da22a8fc
s19497 <s19497@pjwstk.edu.pl>
2021-04-19 17:32:22 +0200

    use let statements in api_spec

--- lab_5_api/api_spec.rb
+++ lab_5_api/api_spec.rb
@@ -5,44 +5,42 @@ require 'net/http'
 BASE_URL = 'https://goweather.herokuapp.com/weather/'.freeze
 RESPONSE_KEYS = %w[temperature wind description forecast].freeze
 
-describe 'weather-api' do
+describe 'when you request weather forecast from weather-api' do
+  let(:response) { Faraday.get url }
+  let(:response_json) { JSON.parse(response.body) }
 
-  describe 'gdansk' do
-    url = "#{BASE_URL}gdansk"
-    response = Faraday.get url
-    response_json = JSON.parse(response.body)
+  context 'when you include real city' do
+    let(:url) { "#{BASE_URL}gdansk" }
 
-    it 'response status should be equal to 200' do
+    it 'has a response status equal to 200' do
       expect(response.status).to eq 200
     end
 
-    it 'response should contain certain keys' do
+    it 'has a response containing certain keys' do
       expect(response_json.keys).to eq RESPONSE_KEYS
     end
   end
 
-  describe 'thiscitydoesntexist' do
-    url = "#{BASE_URL}thiscitydoesntexist"
-    response = Faraday.get url
-    response_json = JSON.parse(response.body)
+  context 'when you include city that does not exist' do
+    let(:url) { "#{BASE_URL}thiscitydoesntexist" }
 
-    it 'response status should still be 200' do
+    it 'responses with 200 status' do
       expect(response.status).to eq 200
     end
 
-    it 'response should still contain those keys' do
+    it 'responses with certain keys' do
       expect(response_json.keys).to eq RESPONSE_KEYS
     end
 
-    it 'response temperature should be empty' do
+    it 'responses with empty temperature' do
       expect(response_json['temperature']).to be_empty
     end
   end
 
-  describe 'no city' do
-    response = Faraday.get BASE_URL
+  context 'when you do not include any city' do
+    let(:url) { BASE_URL }
 
-    it 'response status should be 404' do
+    it 'responses with 404 status' do
       expect(response.status).to eq 404
     end
   end
